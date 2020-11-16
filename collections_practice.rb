89afd612b4cd11a34e65469c386aa3daa22494ea
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2020-11-16 13:43:06 -0500

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -30,15 +30,14 @@ def swap_elements(array)
     end
 end
 
-def swap_elements_from_to(array, index, destination_index)
-    array.sort do |a,b|
-        if a == array[index] && b == array[destination_index]
-            a = array[destination_index]
-              b = array[index]
-        else
-            0
-        end
-end
+# def swap_elements_from_to(array, index, destination_index)
+#     array.sort do |a,b|
+#         if a == array[index] && b == array[destination_index]
+#             1
+#         else
+#             0
+#         end
+# end
 
 def reverse_array(array)
     array.reverse
